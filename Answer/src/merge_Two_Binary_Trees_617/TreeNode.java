cac80aeb0af63a06f8fb91aef6d187904479bf7b
yumubi <shaman2077c@163.com>
2022-12-15 23:10:35 +0800

    617

---
+++ Answer/src/merge_Two_Binary_Trees_617/TreeNode.java
@@ -0,0 +1,32 @@
+package merge_Two_Binary_Trees_617;
+
+public class TreeNode {
+    int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+
+
+       class Solution {
+           public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+                if(root1 == null && root2 == null) return null;
+                else if(root1 == null || root2 == null) {
+                    if(root1 == null) root1 = root2;
+                }
+                else  root1.val = root1.val + root2.val;
+                root1.left = mergeTrees(root1.left, root2.left);
+                root1.right = mergeTrees(root1.right, root2.right);
+                return root1;
+           }
+
+
+
+
+       }
+}
